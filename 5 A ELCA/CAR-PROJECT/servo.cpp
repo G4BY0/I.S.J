dc651efeb6808f3904be6a04bed185f186f5df13
G4BY <105904708+G4BY0@users.noreply.github.com>
2022-07-28 22:30:15 -0300

    ORDENANDO

--- 5 A ELCA/CAR-PROJECT/servo.cpp
+++
@@ -1,8 +0,0 @@
-//-------------------------------------------//
-//            GABRIEL GUERETA 2022           //
-//            FILE: "SERVO"                  //
-//-------------------------------------------//
-
-//INCLUDING ALL LIBRARIES AND FILES FOR THIS:
-#include <setup.hpp>                  //Fichero personal setup
-//---------------------------------------------------------------------------
